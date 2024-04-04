c0d1913bce4c56c359c4819dc4c895088611972d
Hien Tran <hienkim426@gmail.com>
2024-04-04 16:05:49 -0500

    n/a

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
